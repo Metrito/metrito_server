c70c872b7148fb665d9387db16440fdc79f017e2
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-01-24 23:52:53 -0300

    chore: add catch error in initialize server

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,12 +10,15 @@ const logger = mainLogger.context('SERVER', chalk.cyan);
 const port = process.env.PORT;
 
 /**
- * Server initialization.
+ * Server Initialization
  *
- * When starting the server, tests are carried out on vital resources that are
- * essential for the complete functioning of the server.
+ * This script initializes the server, performing essential checks on vital resources
+ * to ensure the complete and smooth functioning of the application.
+ *
+ * The process includes starting the server, performing necessary tests, and logging
+ * information about the server status.
  */
-async function main() {
+async function initializeServer() {
   logger.info('Starting server...');
 
   await listen(app, port);
@@ -23,4 +26,11 @@ async function main() {
   logger.info(`Server is running on port ${chalk.yellow(port)}`);
 }
 
-main();
+initializeServer().catch((error) => {
+  logger.error(chalk.red('Error occurred during server startup:'));
+  logger.error(error);
+
+  setTimeout(() => {
+    process.exit(1);
+  }, 3000); // 3 seconds to save the error in the logs
+});
