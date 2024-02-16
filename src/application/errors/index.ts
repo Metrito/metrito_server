975a4698fdd7a479d51a5cb3ace5e8324e71838a
Yanzaum <yandavid80@gmail.com>
2024-02-16 09:11:53 -0300

    feat: create errors

---
+++ src/application/errors/index.ts
@@ -0,0 +1,6 @@
+export * from './AccessDeniedError';
+export * from './EmailInUseError';
+export * from './InvalidParamError';
+export * from './MissingParamError';
+export * from './ServerError';
+export * from './UnauthorizedError';
